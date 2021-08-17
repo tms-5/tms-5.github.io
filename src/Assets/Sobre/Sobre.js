6862453104c505799341e5ac828d81b2f730eb8f
Thamires Morais <tms5@cin.ufpe.br>
2021-08-17 09:44:15 -0300

    troca de pdf

---
+++ src/Assets/Sobre/Sobre.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function Sobre(){
+
+return(
+    <div className="bg-black font-barlow text-center h-100v d-grid" id="Sobre">   
+
+    <div className="centralizar-meio">        
+
+         <div className="text-start w-75 c-white m-auto">
+             <div className="c-white f-2 text-center mb-5">&lt; SOBRE /&gt;</div> 
+             <div className="c-gray-claro">
+                  <p> Olá! Seja bem-vindo(a).
+            Meu nome é Thamires Morais, tenho 22 anos, sou uma pessoa dinâmica, extrovertida e comunicativa. Meus hobbies são ler, sair, jogar e fazer cursos na área de <span className="c-purple m-0">Front-end</span>.
+                </p>
+                <p>Sou discente em Sistemas de Informação pela Universidade Federal de Pernambuco (UFPE). Atualmente estou no 4º período, e fiz a transferência interna do curso
+                    de Ciência da computação. Meu foco é na área de UI e desenvolvimento front, além de trabalhar como <span className="c-purple m-0">freelancer</span> de <span className="c-purple m-0">Design gráfico</span> para mídias sociais e alguns 
+                    <span className="c-purple m-0"> website</span>.</p>
+
+                <p>Já participei de um hackaton voltado para a área de educação, onde desenvolvemos um website adaptados para pessoas deficientes visuais, e ganhamos em primeiro
+                    lugar. Atualmente trabalho como engenheira de software <span className="c-purple m-0">Front-end</span> na Avanade.
+                </p>
+             </div>
+       
+        </div>
+        </div>    
+       
+        
+    </div>
+);
+}
